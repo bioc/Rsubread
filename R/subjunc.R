1efcee364dd9586a661c60e63ca6e4bcb5fd2917
Wei Shi <shi@wehi.edu.au>
2012-02-14 05:37:46 +0000

    bug fix to subjunc function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsubread@62765 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/subjunc.R
+++ R/subjunc.R
@@ -1,9 +1,9 @@
 subjunc <- function(index,samfile,output_file,paired_end=FALSE,nthreads=1,indels=5,min_distance=50,max_distance=600,PE_orientation="fr")
 {
-	opt <- paste("-i",index,"-o",output_file,"-T",nthreads,"-I",indels,"--nofull","--extending","-H",2,sep=",")
+	opt <- paste("-i",index,"-o",output_file,"-T",nthreads,"-I",indels,"-H",2,"--nofull","--extending",sep=",")
 
 	if(paired_end) 
-	  opt <- paste(opt,"--pairedSAM",samfile,"-d",min_distance,"-D",max_distance,"-S",PE_orientation,sep=",")
+	  opt <- paste(opt,"--pairedSAM",samfile,"-d",min_distance,"-D",max_distance,sep=",")
 	else
 	  opt <- paste(opt,"--singleSAM",samfile,sep=",")
 
