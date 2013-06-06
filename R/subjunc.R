3fb2e8be508a014c09edba425c1e8c82f798670d
Wei Shi <shi@wehi.edu.au>
2013-06-06 06:31:42 +0000

    incorporated changes made in subread 1.3.5
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsubread@77235 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/subjunc.R
+++ R/subjunc.R
@@ -1,9 +1,9 @@
-subjunc <- function(index,samfile,output_file,nsubreads=14,paired_end=FALSE,nthreads=1,indels=5,min_distance=50,max_distance=600,PE_orientation="fr")
+subjunc <- function(index,samfile,output_file,nsubreads=14,paired_end=FALSE,nthreads=1,indels=5,minFragLength=50,maxFragLength=600,PE_orientation="fr")
 {
 	opt <- paste("-i",index,"-o",output_file,"-n",nsubreads,"-T",nthreads,"-I",indels,sep=",")
 
 	if(paired_end) 
-	  opt <- paste(opt,"-2",samfile,"-d",min_distance,"-D",max_distance,sep=",")
+	  opt <- paste(opt,"-2",samfile,"-d",minFragLength,"-D",maxFragLength,sep=",")
 	else
 	  opt <- paste(opt,"-1",samfile,sep=",")
 
