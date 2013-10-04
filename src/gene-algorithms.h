b06e1b61074f37ae2ac84c3a06663c029ca457c7
Yang Liao <liao@wehi.edu.au>
2013-10-04 06:11:34 +0000

    unused methods in gene-aglorithms.h were removed to remove warnings from R
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsubread@81142 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/gene-algorithms.h
+++ src/gene-algorithms.h
@@ -42,8 +42,8 @@ int remove_repeated_reads(gehash_t * table, gehash_t * huge_table,int index_thre
 #define init_gene_vote(a) {memset((a)->items, 0, GENE_VOTE_TABLE_SIZE*sizeof(char)); (a)->max_vote = 0; (a) -> max_indel_recorder[0]=0; (a)->max_tmp_indel_recorder = NULL; (a)->max_mask = 0; (a) -> all_used_subreads = 0; (a) -> noninformative_subreads = 0; }
 // return current votes for a given position
 // if create_new_pos == 0 then do not take this position if it does not exist in the vote array
-//inline void add_gene_vote(gene_vote_t* vote, int position, int create_new_pos);
-//inline void add_gene_vote_weighted(gene_vote_t* vote, int position, int create_new_pos, int w);
+void add_gene_vote(gene_vote_t* vote, int position, int create_new_pos);
+void add_gene_vote_weighted(gene_vote_t* vote, int position, int create_new_pos, int w);
 
 // return the votes; the position is put into position_result 
 int max_gene_vote(gene_vote_t* vote, int * position_result, int query_id);
