cb6c4b4d3db5c2889e6ee201104da24e41e2c0be
Wei Shi <shi@wehi.edu.au>
2013-10-04 03:59:38 +0000

    changes to featureCounts
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsubread@81132 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/featureCounts.R
+++ R/featureCounts.R
@@ -1,4 +1,4 @@
-featureCounts <- function(files,file.type="SAM",annot.inbuilt="mm9",annot.ext=NULL,isGTFAnnotationFile=FALSE,GTF.featureType="exon",GTF.attrType="gene_id",useMetaFeatures=TRUE,allowMultiOverlap=FALSE,nthreads=1,strandSpecific=0,countMultiMappingReads=FALSE,minMQS=0,isPairedEnd=FALSE,requireBothEndsMapped=FALSE,checkFragLength=FALSE,minFragLength=50,maxFragLength=600,countChimericFragments=TRUE)
+featureCounts <- function(files,file.type="SAM",annot.inbuilt="mm9",annot.ext=NULL,isGTFAnnotationFile=FALSE,GTF.featureType="exon",GTF.attrType="gene_id",useMetaFeatures=TRUE,allowMultiOverlap=FALSE,nthreads=1,strandSpecific=0,countMultiMappingReads=FALSE,minMQS=0,isPairedEnd=FALSE,requireBothEndsMapped=FALSE,checkFragLength=FALSE,minFragLength=50,maxFragLength=600,countChimericFragments=TRUE,PEReadsReordering=FALSE,chrAliases="")
 {
 	flag <- FALSE
 
@@ -41,34 +41,28 @@ featureCounts <- function(files,file.type="SAM",annot.inbuilt="mm9",annot.ext=NU
 
 	fout <- file.path(".",paste(".Rsubread_featureCounts_pid",Sys.getpid(),sep=""))
 
-	for(i in 1:length(files)){
-	  cat("Processing", files[i], " ...\n")
-	  cmd <- paste("readSummary",ann,files[i],fout,as.numeric(isPairedEnd),minFragLength,maxFragLength,as.numeric(tolower(file.type)=="sam"),as.numeric(allowMultiOverlap),as.numeric(useMetaFeatures),nthreads,as.numeric(isGTFAnnotationFile),strandSpecific,as.numeric(FALSE),as.numeric(requireBothEndsMapped),as.numeric(!countChimericFragments),as.numeric(checkFragLength),GTF.featureType,GTF.attrType,minMQS,as.numeric(countMultiMappingReads),sep=",")
-	  n <- length(unlist(strsplit(cmd,",")))
-	  C_args <- .C("R_readSummary_wrapper",as.integer(n),as.character(cmd),PACKAGE="Rsubread")
-	  x1 <- read.delim(fout,stringsAsFactors=FALSE)  
-	  if(i == 1)
-	    x <- x1
-	  else
-	    x <- cbind(x,x1$nreads)
-	}
-	colnames(x)[1:5] <- c("GeneID","Chr","Start","End","Strand")
-	colnames(x)[-c(1:5)] <- files
+	files_C <- paste(files,collapse=";")
+
+	cmd <- paste("readSummary",ann,files_C,fout,as.numeric(isPairedEnd),minFragLength,maxFragLength,as.numeric(tolower(file.type)=="sam"),as.numeric(allowMultiOverlap),as.numeric(useMetaFeatures),nthreads,as.numeric(isGTFAnnotationFile),strandSpecific,as.numeric(FALSE),as.numeric(requireBothEndsMapped),as.numeric(!countChimericFragments),as.numeric(checkFragLength),GTF.featureType,GTF.attrType,minMQS,as.numeric(countMultiMappingReads),chrAliases,"",as.numeric(PEReadsReordering),sep=",")
+	n <- length(unlist(strsplit(cmd,",")))
+	C_args <- .C("R_readSummary_wrapper",as.integer(n),as.character(cmd),PACKAGE="Rsubread")
+
+	x <- read.delim(fout,stringsAsFactors=FALSE)  
+
+	colnames(x)[1:6] <- c("GeneID","Chr","Start","End","Strand","Length")
 
 	file.remove(fout)
 	if(flag) 
 	  file.remove(fout_annot)
-
-	if(useMetaFeatures){
-	  y <- rowsum(x[,-c(1:5)],x$GeneID)
-	  gene_length <- rowsum(x$End-x$Start+1,x$GeneID)
-	  z <- list(counts=as.matrix(y),annotation=data.frame(GeneID=rownames(y),Length=gene_length,stringsAsFactors=FALSE),targets=files)
+	
+	if(ncol(x) == 6){
+	  stop("No count data were generated.")
 	}
-	else{
-	  y <- as.matrix(x[,-c(1:5)]) 
-	  rownames(y) <- x$GeneID
-	  z <- list(counts=y,annotation=data.frame(x[,1:5],Length=x$End-x$Start+1,stringsAsFactors=FALSE),targets=files)
-	}
-	z	
+	
+	y <- as.matrix(x[,-c(1:6)]) 
+	rownames(y) <- x$GeneID
+	z <- list(counts=y,annotation=x[,1:6],targets=colnames(y))
+	z
+	
 }
 
