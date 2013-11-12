d2e2238a30e052a6613f8cb74723aa1a4732f5da
Yang Liao <liao@wehi.edu.au>
2013-11-12 22:29:21 +0000

    removed warning message from input-file.c
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsubread@83231 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/input-files.c
+++ src/input-files.c
@@ -40,7 +40,7 @@ int is_R_warnned = 0;
 FILE * f_subr_open(const char * fname, const char * mode)
 {
 
-#if defined(__LP64__) || defined(_LP64)
+#if defined(__LP64__) || defined(_LP64) || defined(MACOS)
 		return fopen(fname, mode);
 #else
 		return fopen64(fname, mode);
