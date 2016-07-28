0e69d0c3284a3802826fb680e0b9c8a8b78fa87c
Wei Shi <shi@wehi.edu.au>
2016-07-28 06:17:55 +0000

    coerce file name parameters to be of character type
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rsubread@119704 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/align.R
+++ R/align.R
@@ -1,9 +1,13 @@
-align <- function(index,readfile1,readfile2=NULL,type="rna",input_format="gzFASTQ",output_format="BAM",output_file=paste(readfile1,"subread",output_format,sep="."),nsubreads=10,TH1=3,TH2=1,maxMismatches=3,nthreads=1,indels=5,complexIndels=FALSE,phredOffset=33,unique=TRUE,nBestLocations=1,minFragLength=50,maxFragLength=600,PE_orientation="fr",nTrim5=0,nTrim3=0,readGroupID=NULL,readGroup=NULL,color2base=FALSE,DP_GapOpenPenalty=-1,DP_GapExtPenalty=0,DP_MismatchPenalty=0,DP_MatchScore=2,detectSV=FALSE)
+align <- function(index,readfile1,readfile2=NULL,type="rna",input_format="gzFASTQ",output_format="BAM",output_file=paste(as.character(readfile1),"subread",output_format,sep="."),nsubreads=10,TH1=3,TH2=1,maxMismatches=3,nthreads=1,indels=5,complexIndels=FALSE,phredOffset=33,unique=TRUE,nBestLocations=1,minFragLength=50,maxFragLength=600,PE_orientation="fr",nTrim5=0,nTrim3=0,readGroupID=NULL,readGroup=NULL,color2base=FALSE,DP_GapOpenPenalty=-1,DP_GapExtPenalty=0,DP_MismatchPenalty=0,DP_MatchScore=2,detectSV=FALSE)
 {
+    readfile1 <- as.character(readfile1)
+    output_file <- as.character(output_file)
+    
 	if(length(readfile1) != length(output_file))
 		stop("The number of input file names is different from the number of output file names.")
 
 	if(!is.null(readfile2)){
+        readfile2 <- as.character(readfile2)
 		if(length(readfile1) != length(readfile2))
 			stop("The number of file names for the first reads is different from the number of file names for the second reads.")
 	}
