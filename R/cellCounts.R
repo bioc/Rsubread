2bfef4f8a755d085946003086a30b007008bb5df
Yang Liao <dbpzdbpz@gmail.com>
2019-08-21 16:12:33 +1000

    fixed a typo

--- R/cellCounts.R
+++ R/cellCounts.R
@@ -6,7 +6,7 @@ cellCounts <- function(input.directory, index, output.BAM, sample.sheet, cell.ba
 	  input.1 <- input.directory[ii]
 	  output.1 <- output.BAM[ii]
 	  sample.1 <- sample.sheet[ii]
-	  align(index, input.1, output_file=output.1, nthread=nthreads, isBCLinput=TRUE)
+	  align(index, input.1, output_file=output.1, nthreads=nthreads, isBCLinput=TRUE)
 	  fc[[paste0("counts.", ob)]]<-featureCounts(output.1, annot.inbuilt=annot.inbuilt, annot.ext=annot.ext, isGTFAnnotationFile=isGTFAnnotationFile, GTF.featureType=GTF.featureType, GTF.attrType=GTF.attrType, GTF.attrType.extra=GTF.attrType.extra, chrAliases=chrAliases, useMetaFeatures=useMetaFeatures, allowMultiOverlap=allowMultiOverlap, countMultiMappingReads=countMultiMappingReads, sampleSheet=sample.1, cellBarcodeList=cell.barcode.list)
   }
   for(ob in output.BAM){
