9751e07218a6832a1fcedb52ac69fb536a0abb6d
Yang Liao <user.name>
2020-07-24 09:16:03 +1000

    Fix errors in gcc10

--- src/detectionCallAnnotation.c
+++ src/detectionCallAnnotation.c
@@ -48,16 +48,16 @@ typedef struct a_chr{
 
 /* Variables */
 int binsize;
-chr exon[MAXCHRNUM];
-chr ir[MAXCHRNUM];
+chr DCC_exon[MAXCHRNUM];
+chr DCC_ir[MAXCHRNUM];
 
-int chr_num;
-char current_chr_id[STR];
+int DCC_chr_num;
+char DCC_current_chr_id[STR];
 
 char *utr_cds_file;
-char *exon_file;
-char *ir_file;
-char *binned_ir_file;
+char *DCC_exon_file;
+char *DCC_ir_file;
+char *binned_DCC_ir_file;
 char *fa_header;
 
 
@@ -76,26 +76,26 @@ make_empty_node(void){
 	return new;
 }
 
-/*cheating function, build exon data structure from exon file*/
+/*cheating function, build DCC_exon data structure from DCC_exon file*/
 void
-build_exon_data_structure(void){
+build_DCC_exon_data_structure(void){
 	FILE *fin;
 	int read_start, read_end, read_entrezid;
 	char chr_id[STR];
 	node *cnode=NULL;
-	char current_chr_id[STR];
+	char DCC_current_chr_id[STR];
 
-	chr_num = 0;
-	fin = fopen(exon_file,"r");
+	DCC_chr_num = 0;
+	fin = fopen(DCC_exon_file,"r");
 	while  (fscanf(fin, "%d %s %d %d",&read_entrezid, chr_id, &read_start, &read_end) != -1){
-		if (strcmp(chr_id, current_chr_id) != 0){
-			strcpy(current_chr_id, chr_id);
-			chr_num++;
+		if (strcmp(chr_id, DCC_current_chr_id) != 0){
+			strcpy(DCC_current_chr_id, chr_id);
+			DCC_chr_num++;
 			//printf("new chromosome is: %s\n", chr_id);
-			exon[chr_num-1].id = (char *)malloc(STR);
-			strcpy(exon[chr_num-1].id, chr_id);
-			exon[chr_num-1].list = (node *)make_empty_node();
-			cnode = exon[chr_num-1].list;
+			DCC_exon[DCC_chr_num-1].id = (char *)malloc(STR);
+			strcpy(DCC_exon[DCC_chr_num-1].id, chr_id);
+			DCC_exon[DCC_chr_num-1].list = (node *)make_empty_node();
+			cnode = DCC_exon[DCC_chr_num-1].list;
 		}
 
 		node *new_node;
@@ -112,23 +112,23 @@ build_exon_data_structure(void){
 
 /* Build from binned integenic region */
 void
-build_ir_data_structure(void){
+build_DCC_ir_data_structure(void){
 	FILE *fin;
 	int read_start, read_end;
 	char chr_id[STR];
 	node *cnode=NULL;
-	char current_chr_id[STR];
+	char DCC_current_chr_id[STR];
 
-	chr_num = 0;
-	fin = fopen(binned_ir_file,"r");
+	DCC_chr_num = 0;
+	fin = fopen(binned_DCC_ir_file,"r");
 	while  (fscanf(fin, "%s %d %d",chr_id, &read_start, &read_end) != -1){
-		if (strcmp(chr_id, current_chr_id) != 0){
-			strcpy(current_chr_id, chr_id);
-			chr_num++;
-			ir[chr_num-1].id = (char *)malloc(STR);
-			strcpy(ir[chr_num-1].id, chr_id);
-			ir[chr_num-1].list = (node *)make_empty_node();
-			cnode = ir[chr_num-1].list;
+		if (strcmp(chr_id, DCC_current_chr_id) != 0){
+			strcpy(DCC_current_chr_id, chr_id);
+			DCC_chr_num++;
+			DCC_ir[DCC_chr_num-1].id = (char *)malloc(STR);
+			strcpy(DCC_ir[DCC_chr_num-1].id, chr_id);
+			DCC_ir[DCC_chr_num-1].list = (node *)make_empty_node();
+			cnode = DCC_ir[DCC_chr_num-1].list;
 		}
 
 		node *new_node;
@@ -152,8 +152,8 @@ breakIntegenicRegion(){
 	int read_start, read_end;
 	char chr_id[STR];
 	int binnum = 0;
-	fin = fopen(ir_file, "r");
-	fout = fopen(binned_ir_file, "w");
+	fin = fopen(DCC_ir_file, "r");
+	fout = fopen(binned_DCC_ir_file, "w");
 
 	while (fscanf(fin, "%s %d %d", chr_id, &read_start, &read_end) != -1){
 
@@ -184,10 +184,10 @@ calculateExonGCContent(void){
 	int linenum;
 	char *filename;
 
-	for (i=0; i<chr_num; i++){
+	for (i=0; i<DCC_chr_num; i++){
 		filename = (char *)malloc(STR*sizeof(char));
 		strcpy(filename, fa_header);
-		strcat(filename, exon[i].id);
+		strcat(filename, DCC_exon[i].id);
 		strcat(filename,".fa");
 		// printf("Exon GC content: the fa file is: %s\n", filename);
 
@@ -196,7 +196,7 @@ calculateExonGCContent(void){
 		char * flin = fgets(line, sizeof(line), fseq);
 		if(NULL == flin) break;
 
-		bin = exon[i].list->next;
+		bin = DCC_exon[i].list->next;
 		pos = 0;
 		linenum = 0;
 		while ( fgets ( line, sizeof(line), fseq) != NULL ){
@@ -229,10 +229,10 @@ calculateExonGCContent(void){
 		} // end-while
 	} // end-for
 
-	/*remove heavyN exons*/
+	/*remove heavyN DCC_exons*/
 	node *to_remove;
-	for (i=0; i<chr_num; i++){
-		bin = exon[i].list;
+	for (i=0; i<DCC_chr_num; i++){
+		bin = DCC_exon[i].list;
 		while (bin->next != NULL){
 			if ((1.0*bin->next->nnum/(bin->next->end - bin->next->start + 1)) > NPERCENTAGE){
 				// this bin contains too much N, remove it
@@ -248,18 +248,18 @@ calculateExonGCContent(void){
 
 	/* Output result to file*/
 	
-	char *exon_GC_file = "exon_GC.txt";
-	FILE *f_annotation_exon = fopen(exon_GC_file, "w");
+	char *DCC_exon_GC_file = "exon_GC.txt";
+	FILE *f_annotation_DCC_exon = fopen(DCC_exon_GC_file, "w");
 	node *dh;
-	//fprintf(f_annotation_exon,"entrezid\tchr\tstart\tend\tnnum\tgcnum\tatnum\n");
-	for(i=0; i<chr_num; i++){
-		dh = exon[i].list;
+	//fprintf(f_annotation_DCC_exon,"entrezid\tchr\tstart\tend\tnnum\tgcnum\tatnum\n");
+	for(i=0; i<DCC_chr_num; i++){
+		dh = DCC_exon[i].list;
 		while (dh->next != NULL){
 			dh = dh->next;
-			fprintf(f_annotation_exon, "%d\t%s\t%d\t%d\t%d\t%d\t%d\n", dh->gene, exon[i].id, dh->start, dh->end, dh->nnum, dh->gcnum, dh->atnum);
+			fprintf(f_annotation_DCC_exon, "%d\t%s\t%d\t%d\t%d\t%d\t%d\n", dh->gene, DCC_exon[i].id, dh->start, dh->end, dh->nnum, dh->gcnum, dh->atnum);
 		}
 	}
-	fclose(f_annotation_exon);
+	fclose(f_annotation_DCC_exon);
 }
 
 void
@@ -274,10 +274,10 @@ calculateIRGCContent(void){
 	int linenum;
 	char *filename;
 
-	for (i=0; i<chr_num; i++){
+	for (i=0; i<DCC_chr_num; i++){
 		filename = (char *)malloc(STR*sizeof(char));
 		strcpy(filename, fa_header);
-		strcat(filename, ir[i].id);
+		strcat(filename, DCC_ir[i].id);
 		strcat(filename,".fa");
 		// printf("IR GC content: the fa file is: %s\n", filename);
 
@@ -285,7 +285,7 @@ calculateIRGCContent(void){
 		char * flin = fgets(line, sizeof(line), fseq);
 		if(NULL == flin) break;
 
-		bin = ir[i].list->next;
+		bin = DCC_ir[i].list->next;
 		pos = 0;
 		linenum = 0;
 		while ( fgets ( line, sizeof(line), fseq) != NULL ){
@@ -320,8 +320,8 @@ calculateIRGCContent(void){
 
 	/*remove heavyN integenic region bins*/
 	node *to_remove;
-	for (i=0; i<chr_num; i++){
-		bin = ir[i].list;
+	for (i=0; i<DCC_chr_num; i++){
+		bin = DCC_ir[i].list;
 		while (bin->next != NULL){
 			if ((1.0*bin->next->nnum/(bin->next->end - bin->next->start + 1)) > NPERCENTAGE){
 				// this bin contains too much N, remove it
@@ -336,18 +336,18 @@ calculateIRGCContent(void){
 
 
 	/* Output result to file*/
-	char *ir_GC_file = "binned_integenic_region_GC.txt";
-	FILE *f_annotation_ir = fopen(ir_GC_file, "w");
+	char *DCC_ir_GC_file = "binned_integenic_region_GC.txt";
+	FILE *f_annotation_DCC_ir = fopen(DCC_ir_GC_file, "w");
 	node *dh;
-	//fprintf(f_annotation_ir,"chr\tstart\tend\tnnum\tgcnum\tatnum\n");
-	for(i=0; i<chr_num; i++){
-		dh = ir[i].list;
+	//fprintf(f_annotation_DCC_ir,"chr\tstart\tend\tnnum\tgcnum\tatnum\n");
+	for(i=0; i<DCC_chr_num; i++){
+		dh = DCC_ir[i].list;
 		while (dh->next != NULL){
 			dh = dh->next;
-			fprintf(f_annotation_ir, "%s\t%d\t%d\t%d\t%d\t%d\n", ir[i].id, dh->start, dh->end, dh->nnum, dh->gcnum, dh->atnum);
+			fprintf(f_annotation_DCC_ir, "%s\t%d\t%d\t%d\t%d\t%d\n", DCC_ir[i].id, dh->start, dh->end, dh->nnum, dh->gcnum, dh->atnum);
 		}
 	}
-	fclose(f_annotation_ir);
+	fclose(f_annotation_DCC_ir);
 
 }
 
@@ -358,42 +358,42 @@ DetectionCallAnnotationBody(void){
 	breakIntegenicRegion();
 	// printf("finsihed break into binss\n");
 	
-	build_ir_data_structure();
-	//printf("The chr number after building ir structure is : %d\n", chr_num);
+	build_DCC_ir_data_structure();
+	//printf("The chr number after building DCC_ir structure is : %d\n", DCC_chr_num);
 	calculateIRGCContent();
 
-	build_exon_data_structure();
-	//printf("The chr number after building exon structure is : %d\n", chr_num);
+	build_DCC_exon_data_structure();
+	//printf("The chr number after building DCC_exon structure is : %d\n", DCC_chr_num);
 	calculateExonGCContent();
 
 }
 
 
 void
-detectionCallAnnotation(char **exonfile, char **irfile, char **species, int *user_binsize){
+detectionCallAnnotation(char **DCC_exonfile, char **DCC_irfile, char **species, int *user_binsize){
 
 	// initialisation
 	
-	/* exon and ir files are pre-processed without unwanted chromosome data */
-	exon_file = (char *)malloc(STR);
-	strcpy(exon_file, *exonfile);
-	ir_file = (char *)malloc(STR);
-	strcpy(ir_file, *irfile);
+	/* DCC_exon and DCC_ir files are pre-processed without unwanted chromosome data */
+	DCC_exon_file = (char *)malloc(STR);
+	strcpy(DCC_exon_file, *DCC_exonfile);
+	DCC_ir_file = (char *)malloc(STR);
+	strcpy(DCC_ir_file, *DCC_irfile);
 	fa_header = (char *)malloc(STR);
 
-	binned_ir_file = (char *)malloc(STR);
+	binned_DCC_ir_file = (char *)malloc(STR);
 	if (strcmp(*species, "hg") == 0){
-		strcpy(binned_ir_file, "hg19_binned_integenic_region.txt");
+		strcpy(binned_DCC_ir_file, "hg19_binned_integenic_region.txt");
 		strcpy(fa_header,"human_sequence_data/hs_ref_GRCh37_");
 	} 
 	if (strcmp(*species, "mm") == 0){
-		strcpy(binned_ir_file, "mm9_binned_integenic_region.txt");
+		strcpy(binned_DCC_ir_file, "mm9_binned_integenic_region.txt");
 		strcpy(fa_header, "mouse_sequence_data/");
 	} 
 	binsize = *user_binsize;
 	// printf("going to main body of function\n");
 	DetectionCallAnnotationBody();
 
-	/*  printf("\nUSAGE: DetectionCallAnnotation(exonFilename, integenicRegionFilename, binsizse)\n\n");
-		printf("BY DEFAULT:     exonFilename = exon.txt\n\t\tintegenicRegionFilename = integenic_region.txt\n\t\tbinsize = 2000\n\n");	*/
+	/*  printf("\nUSAGE: DetectionCallAnnotation(DCC_exonFilename, integenicRegionFilename, binsizse)\n\n");
+		printf("BY DEFAULT:     DCC_exonFilename = DCC_exon.txt\n\t\tintegenicRegionFilename = integenic_region.txt\n\t\tbinsize = 2000\n\n");	*/
 }
