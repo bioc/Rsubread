f18437654a8ff210885cb5ec1e42e01c7b994891
Yang Liao <dbpzdbpz@gmail.com>
2020-08-24 07:51:48 +1000

    Fixed a bug

--- R/cellCounts.R
+++ R/cellCounts.R
@@ -330,7 +330,7 @@ cellCounts <- function(index, input.directory, output.BAM, sample.sheet, cell.ba
 	  }
       raw.fc<-featureCounts(output.1, annot.inbuilt=annot.inbuilt, annot.ext=annot.ext, isGTFAnnotationFile=isGTFAnnotationFile, GTF.featureType=GTF.featureType, GTF.attrType=GTF.attrType, GTF.attrType.extra=GTF.attrType.extra, chrAliases=chrAliases, useMetaFeatures=useMetaFeatures, allowMultiOverlap=allowMultiOverlap, countMultiMappingReads=countMultiMappingReads, sampleSheet=sample.1, cellBarcodeList=cell.barcode.list, nthreads=nthreads)
 	  fc[[paste0("Dataset.", ii)]] <- .load.all.scSamples(output.1, as.character(fc[[paste0("counts.", ii)]]$annotation$GeneID))
-      if(is.null(raw.fc.annot)) raw.fc.annot<-raw.fc$annotations
+      if(is.null(raw.fc.annot)) raw.fc.annot<-raw.fc$annotation
   }
   fc[["Input.Files"]] <- input.directory
   fc[["Annotations"]] <- raw.fc.annot
