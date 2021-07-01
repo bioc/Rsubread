909401117ba5324296546f5ff32298e9423e85be
Yang Liao <liao_y@svr2672.licr.unimelb.edu.au>
2021-07-01 13:18:38 +1000

    typo in featureCounts.R

--- R/featureCounts.R
+++ R/featureCounts.R
@@ -9,10 +9,10 @@
 }
 
 .is.64bit.system <- function(){
-    if(grepl("[-_]64",Sys.info()[["machine"]]))return TRUE
-    if(grepl("aarch64",Sys.info()[["machine"]]))return TRUE
+    if(grepl("[-_]64",Sys.info()[["machine"]]))return(TRUE)
+    if(grepl("aarch64",Sys.info()[["machine"]]))return(TRUE)
     
-    return FALSE
+    return(FALSE)
 }
 
 .check_string_param <- function(argu, opt=NULL){
