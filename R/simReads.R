60f055f12caec5f4ba39a8a503fa1137ea88d102
Gordon Smyth <smyth@wehi.edu.au>
2022-10-15 17:10:57 +1100

    Rsubread 2.11.5 (1) Reduce default library.size in simReads() from 1e6 to 1e5. (2) More error checking for simReads(). (3) Minor revisions to simReads() help page.

--- R/simReads.R
+++ R/simReads.R
@@ -1,4 +1,4 @@
-.simFragments <- function(transcript.lengths, transcript.expressions=NULL, library.size=1e6, fragment.length.min=100L, fragment.length.max=500L, fragment.length.mean=180, fragment.length.sd=40)
+.simFragments <- function(transcript.lengths, transcript.expressions=NULL, library.size=1000000L, fragment.length.min=100L, fragment.length.max=500L, fragment.length.mean=180, fragment.length.sd=40)
 #  Randomly generate fragment lengths and starting positions for RNA-seq simulation.
 #  Assume gamma distribution for fragment lengths.
 #  Gordon Smyth
@@ -15,7 +15,7 @@
 # First generate number of fragments from each transcript
 
 # We will assume that any transcript with length >= fragment.length.min has a chance of being sequenced
-  effective.transcript.lengths <- pmax(transcript.lengths - fragment.length.min + 1, 0)  
+  effective.transcript.lengths <- pmax(transcript.lengths - fragment.length.min + 1L, 0L)  
   prob <- effective.transcript.lengths * transcript.expressions
   prob <- prob / sum(prob)
 
@@ -61,26 +61,39 @@
   list(n.fragments=n.fragments, read.positions=out)
 }
 
-simReads <- function(transcript.file, expression.levels, output.prefix, library.size=1000000, read.length=75, truth.in.read.names=FALSE, simulate.sequencing.error=TRUE, quality.reference=NULL, paired.end=FALSE, fragment.length.min=100L, fragment.length.max=500L, fragment.length.mean=180, fragment.length.sd=40, simplify.transcript.names=FALSE){
-  expression.levels[ is.na(expression.levels) ]<-0
-  if(fragment.length.min < read.length)stop("The minimum fragment length cannot be lower than the output read length.")
-  if(read.length<1 || read.length>250)stop("The output read length must be between 1 and 250.")
-  if(library.size<1) stop("At least one read should be generated.")
-  if(any(expression.levels<0))stop("No negative expression values are allowed.")
-  if(sum(expression.levels)==0)stop("The wanted expression levels are all zero.")
+simReads <- function(transcript.file, expression.levels, output.prefix, library.size=1e5, read.length=75L, truth.in.read.names=FALSE, simulate.sequencing.error=TRUE, quality.reference=NULL, paired.end=FALSE, fragment.length.min=100L, fragment.length.max=500L, fragment.length.mean=180, fragment.length.sd=40, simplify.transcript.names=FALSE)
+# Simulate transcript reads and write FASTQ files
+{
+# Check expression.levels
+  if(anyNA(expression.levels)) expression.levels[ is.na(expression.levels) ] <- 0
+  if(min(expression.levels) < 0) stop("Negative expression values are not allowed.")
+  if(max(expression.levels) == 0) stop("The specified transcript expression levels are all zero.")
 
-  if(library.size>100*1000*1000) stop("The current version cannot generate more than 100 million reads/fragments in a single run")
+# Check library.size
+  if(!is.integer(library.size)) library.size <- as.integer(library.size+0.5)
+  if(library.size < 1L) stop("At least one read should be generated.")
+  if(library.size > 100L*1000L*1000L) stop("The current version cannot generate more than 100 million reads/fragments in a single run")
+
+# Check read length
+  if(!is.integer(read.length)) read.length <- as.integer(read.length+0.5)
+  if(read.length < 1L || read.length > 250L) stop("The read length must be between 1 and 250.")
+
+# Check fragment lengths
+  if(fragment.length.min < read.length) stop("The minimum fragment length cannot be lower than the output read length.")
+  if(fragment.length.min > fragment.length.max) stop("Maximum fragment length must be greater than or equal to the minimum fragment length.")
+  if(fragment.length.mean < 1) stop("Mean fragment length must be >= 1")
+
+# Check transcript.file
   transcript.file <- .check_and_NormPath(transcript.file, mustWork=TRUE, opt="transcript.file")
   output.prefix <- .check_and_NormPath(output.prefix, mustWork=FALSE, opt="output.prefix")
-
-  fasta.meta <- scanFasta(transcript.file, simplify.transcript.names, quiet=T)
-  if(length(expression.levels)!=nrow(fasta.meta))stop("The wanted expression levels do not match the transcripts in the input fasta file.")
+  fasta.meta <- scanFasta(transcript.file, simplify.transcript.names, quiet=TRUE)
+  if(!identical(length(expression.levels),nrow(fasta.meta))) stop("Number of expression levels does not match the number of transcripts in the input fasta file.")
 
   if(simulate.sequencing.error){
     if(is.null(quality.reference)){
       if(read.length==75) quality.reference <- system.file("qualf","ref-quality-strings-20k-75bp-ERR1_59-SRR3649332.txt",package="Rsubread")
       if(read.length==100) quality.reference <- system.file("qualf","ref-quality-strings-20k-100bp-ERR2_70-SRR3045231.txt",package="Rsubread")
-      if(is.null(quality.reference)) stop("To simulate sequencing errors in reads that are neither 100-bp nor 75-bp long, you need to provide a file containing reference quality strings of the same length as the output reads.")
+      stop("To simulate sequencing errors in reads that are neither 100-bp nor 75-bp long, you need to provide a file containing reference quality strings of the same length as the output reads.")
     }else{
       quality.reference <- .check_and_NormPath(quality.reference,  mustWork=TRUE, opt="quality.reference")
     }
@@ -94,17 +107,17 @@ simReads <- function(transcript.file, expression.levels, output.prefix, library.
 }
 
 scanFasta <- function(transcript.file, simplify.transcript.names=FALSE, quiet=FALSE){
-	fout_sum <- file.path(".",paste(".Rsubread_sumfile_pid",Sys.getpid(),sep=""))
-	transcript.file <- .check_and_NormPath(transcript.file, mustWork=T, opt="transcript.file")
-	cmd <- paste("RscanFasta","--summarizeFasta","--transcriptFasta",transcript.file,"--outputPrefix", fout_sum, sep=.R_param_splitor)
-    if(quiet) cmd<-paste(cmd, "--quiet", sep=.R_param_splitor)
-    if(simplify.transcript.names) cmd<-paste(cmd, "--simpleTranscriptId", sep=.R_param_splitor)
+  fout_sum <- file.path(".",paste(".Rsubread_sumfile_pid",Sys.getpid(),sep=""))
+  transcript.file <- .check_and_NormPath(transcript.file, mustWork=T, opt="transcript.file")
+  cmd <- paste("RscanFasta","--summarizeFasta","--transcriptFasta",transcript.file,"--outputPrefix", fout_sum, sep=.R_param_splitor)
+  if(quiet) cmd<-paste(cmd, "--quiet", sep=.R_param_splitor)
+  if(simplify.transcript.names) cmd<-paste(cmd, "--simpleTranscriptId", sep=.R_param_splitor)
 
-	n <- length(unlist(strsplit(cmd,.R_param_splitor)))
-	C_args <- .C("R_generate_random_RNAseq_reads",as.integer(n), as.character(cmd),PACKAGE="Rsubread")
+  n <- length(unlist(strsplit(cmd,.R_param_splitor)))
+  C_args <- .C("R_generate_random_RNAseq_reads",as.integer(n), as.character(cmd),PACKAGE="Rsubread")
 
-	fout_sum <-paste0(fout_sum,".faSummary")
-	summ <- read.delim(fout_sum, stringsAsFactors=FALSE, header=T)
-	file.remove(fout_sum)
-	summ
+  fout_sum <-paste0(fout_sum,".faSummary")
+  summ <- read.delim(fout_sum, stringsAsFactors=FALSE, header=T)
+  file.remove(fout_sum)
+  summ
 }
