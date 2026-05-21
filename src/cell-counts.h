004d3f02c8732924146917f8722fa7b27b0573f3
Yang Liao (Monash University) <yliao@m3v103.massive.org.au>
2026-05-21 11:40:25 +1000

    added buffer size for io in cellCounts

--- src/cell-counts.h
+++ src/cell-counts.h
@@ -4,7 +4,7 @@
 #ifndef __CELL_COUNTS_H
 #define __CELL_COUNTS_H
 
-#define SCRNA_VBUFF_SIZE (256*1024)
+#define SCRNA_VBUFF_SIZE (32*1024*1024)
 #define MAX_FC_READ_LENGTH 10001
 #define READ_BIN_BUF_SIZE 1000 // sufficient for a <=150bp read.
 #define CELLBC_BATCH_NUMBER 149
