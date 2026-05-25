7798b9ee12b13e7af207aef444d4852ea5c237e5
Yang Liao (Monash University) <yliao@m3v106.massive.org.au>
2026-05-26 09:49:32 +1000

    removed debug output

--- src/cell-counts.c
+++ src/cell-counts.c
@@ -4611,7 +4611,7 @@ static int cellCounts_temp_realign_fp_finish_write(cellcounts_temp_file_point_t 
 
 void cellcounts_temp_file_destroy(cellcounts_global_t * cct_context, char * tmp_fname, cellcounts_temp_file_point_t *temp_fp){
 	if(temp_fp -> realign_temp_memspace){
-		fprintf(stderr,"FREEPTR %p\n", temp_fp -> realign_temp_memspace);
+//		fprintf(stderr,"FREEPTR %p\n", temp_fp -> realign_temp_memspace);
 		free(temp_fp -> realign_temp_memspace);
 		temp_fp -> realign_temp_memspace = NULL;
 	}
@@ -4635,7 +4635,8 @@ void cellcounts_temp_file_open(cellcounts_global_t * cct_context, char * tmp_fna
 	if(cct_context -> cell_level_junction_memory_temp){
 		if(for_writting){
 			temp_fp -> realign_temp_memspace = malloc(TEMP_BINFILE_MEMORY_SIZE_INIT);
-			if(temp_fp -> realign_temp_memspace)fprintf(stderr,"MALLOCED: %p\n", temp_fp -> realign_temp_memspace);else{
+			if(0&& temp_fp -> realign_temp_memspace)fprintf(stderr,"MALLOCED: %p\n", temp_fp -> realign_temp_memspace);
+			if(! temp_fp -> realign_temp_memspace){
 				fprintf(stderr,"\n\nEEROR: NUL BUFF\n\n");
 				fprintf(stderr,"\n\nEEROR: NUL BUFF\n\n");
 				assert(0);
