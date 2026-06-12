1311da3713041fed1b615e6e982a5e65b3ad5032
Yang Liao <yang.liao@onjcri.org.au>
2026-06-12 14:12:59 +1000

    added manual for enableSoftClipping

--- R/cellCounts.R
+++ R/cellCounts.R
@@ -1401,7 +1401,11 @@
   return(sheet)
 }
 
-cellCounts <- function( index, sample, input.mode = "BCL", cell.barcode = NULL, nsubreads = 15, minVotes = 1, maxMismatches = 10, minMappedLength = 1, annot.inbuilt = "mm39", annot.ext = NULL, isGTFAnnotationFile = FALSE, GTF.featureType = "exon", GTF.attrType = "gene_id", useMetaFeatures = TRUE, detectJunctions = FALSE, binaryTempMemory = FALSE, umi.cutoff = NULL, nthreads = 10, nBestLocations = 1, uniqueMapping = FALSE, reportExcludedBarcodes = FALSE, VisiumHD.barcode.file=NULL, enableSoftClipping=!detectJunctions ){
+cellCounts <- function( index, sample, input.mode = "BCL", cell.barcode = NULL, nsubreads = 15,
+		       minVotes = 1, maxMismatches = 10, minMappedLength = 1, enableSoftClipping=!detectJunctions ,
+		       annot.inbuilt = "mm39", annot.ext = NULL, isGTFAnnotationFile = FALSE, GTF.featureType = "exon", GTF.attrType = "gene_id", useMetaFeatures = TRUE,
+		       detectJunctions = FALSE, binaryTempMemory = FALSE, umi.cutoff = NULL,
+		       nthreads = 10, nBestLocations = 1, uniqueMapping = FALSE, reportExcludedBarcodes = FALSE, VisiumHD.barcode.file=NULL){
   if(F)if(!   (   file.exists("/home/vdiuser/Projects/GOlib/DBPZ/go.sum")  ||  file.exists("/fs04/ws30/Liao/Common/Index/Subread/build-index.bash") || file.exists("/home/biocbuild/bbs-3.24-bioc/R/bin/R") ) ){
      stop("The devel version is not for general use. Please install the released version.")
      return(NULL)
